aff1ee58467ef4d0a0d8475b358a1ee5f0b2a4b2
Lex <fischer84@mail.ru>
2022-02-21 18:34:41 +0300

    added pages and hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
+import {useRoutes} from './routes';
 import 'materialize-css';
 
 function App() {
+  const routes = useRoutes(false);
+
   return (
-    <div className='container'>
-      <h1>Hello Alex</h1>
-    </div>
+    <BrowserRouter>
+      <div className='container'>
+        {routes}
+      </div>
+    </BrowserRouter>    
   );
 }
 
